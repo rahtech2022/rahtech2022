bf2fdf057537aaf7607d85b4310c2b2d51ebc24e
rahtech2022 <101383392+rahtech2022@users.noreply.github.com>
2022-12-27 21:25:18 +0330

    works fine

--- ParameterCalculation.m
+++ ParameterCalculation.m
@@ -2,16 +2,16 @@ close all;
 clear all;
 clc;
 
-maximumVelocity=80;% Km/h
+maximumVelocity=63.75;% Km/h
 velocityResolution = 2; % Km/h
 
-maximumRange = 75; % meter
-rangeResolution = 0.5; % meter
+maximumRange = 70; % meter
+rangeResolution = 0.28; % meter
 C=3e8;% m/s
 f=77e9;
 lamda=C/f;
 
-ADCStartTime=3;%(us)
+ADCStartTime=4;%(us)
 TXStartTime=1;%(us)
 idleTime=3;%(us)
 ExtraRampTime=1;
@@ -21,15 +21,26 @@ NumberofRxChannels=4;
 NumberofTxChannels=2;
 
 samplingFrequency=5500;
-%% Duration of One chirp
+%% Duration of One chirp (Chirp Cycle Time)
 maximumVelocity = maximumVelocity/3.6; % m/s
 Tc=lamda/(4*maximumVelocity); % second
 fprintf('Duration of One chirp is (us) %d \n\r',Tc*1e6);
 %% Bandwidth Calculation
 bandwidth =C/(2*rangeResolution);
 fprintf('Required Bandwidth (MHz) %d \n\r',bandwidth*1e-6);
+%% IDLE TIME
+%if bandwidth < 1e9
+    %idleTime=2;%(us)
+%elseif 1e9< bandwidth && bandwidth < 2e9
+    %idleTime=3.5;%(us)
+%elseif 2e9< bandwidth && bandwidth < 3e9
+    %idleTime=5;%(us)
+%else
+    %idleTime=7;%(us)
+%end
+%fprintf('Idle Time is(us) %d \n\r',idleTime);
 %% Slope of LFM
-slope = bandwidth / (Tc);
+slope = bandwidth / (Tc-(idleTime*1e-6));
 fprintf('Frequency Slope (MHz/us) %d \n\r',slope*1e-12);
 %% Frame Time
 velocityResolution = velocityResolution /3.6; % m/s
@@ -57,7 +68,7 @@ numberOfSamples=(adcSamplingTime*1e-6)*(samplingFrequency*1e3);
 numberOfSamples=floor(numberOfSamples);
 fprintf('Number of Samples %d \n\r',numberOfSamples);
 %% Ramp End Time
-ExtraRampTime=0;
+%ExtraRampTime=0;
 RampEndTime=adcSamplingTime+ADCStartTime+ExtraRampTime;%us
 fprintf('Ramp End Time is %d \r\n',round(RampEndTime));
 %% Number of Chirps in the frame
